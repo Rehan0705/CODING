be74360b71d3999d0de9cead1d1d464b6f171a84
Rehan0705 <mohammadrazak876@gmail.com>
2022-06-20 13:49:01 +0530

    Update SwapTwoNumbers.cpp
    s

--- SwapTwoNumbers.cpp
+++ SwapTwoNumbers.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int num1=5, num2=10, temp=0;
+    cout<<"before swapping."<<endl;
+    cout<<"num1="<<num1<<", num2="<<num2<<endl;
+    temp=num1;
+    num1=num2;
+    num2=temp;
+    cout<<"after swapping."<<endl;
+    cout<<"num1="<<num1<<", num2="<<num2<<endl;
+
+    return 0; 
+}
