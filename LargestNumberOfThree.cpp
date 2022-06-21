2c51b5fcf47b7dee9f5954875da94fc109e50533
Rehan0705 <mohammadrazak876@gmail.com>
2022-06-21 19:24:38 +0530

    Update LargestNumberOfThree.cpp
    e

--- LargestNumberOfThree.cpp
+++ LargestNumberOfThree.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    double n1 ,n2, n3;
+    cout<<"enter the numbers:";
+    cin>>n1>>n2>>n3;
+    if((n1>=n2)&&(n1>=n3))
+        cout<<"largest number :"<<n1;
+    else if((n2>=n1)&&(n2>=n3))
+        cout<<"largest number :"<<n2;
+    else
+        cout<<"largest number :"<<n3;
+
+    return 0;
+}
