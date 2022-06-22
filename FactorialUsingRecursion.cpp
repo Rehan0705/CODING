edc04998a06b2099d9a3632acedd0846008fccb1
Rehan0705 <mohammadrazak876@gmail.com>
2022-06-22 17:40:14 +0530

    Update FactorialUsingRecursion.cpp
    f

--- FactorialUsingRecursion.cpp
+++ FactorialUsingRecursion.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+using namespace std;
 
+int factorial(int n)
+{
+    if(n>1)
+        return n*factorial(n-1);
+    else
+        return 1;
+
+}
+int main()
+{
+    int n;
+    cout<<"enter a positive integer:";
+    cin>>n;
+    cout<<"factorial of"<<n<<"="<<factorial(n);
+
+    return 0;
+}
