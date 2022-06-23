ed547d3075928dd5eb5be4b716f70b87947af9a7
Rehan0705 <mohammadrazak876@gmail.com>
2022-06-23 13:09:20 +0530

    Update SmallestAndSecondSmallestNumber.cpp
    ss

--- SmallestAndSecondSmallestNumber.cpp
+++ SmallestAndSecondSmallestNumber.cpp
@@ -1,1 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int arr[100],n;
+    cout<<"enter number of elements:";
+    cin>>n;
+    cout<<"\n enter array:";
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    sort(arr,arr+n);
+    cout<<"smallest number :"<<arr[0]<<"\n second smallest number :"<<arr[1]<<endl;
+
+    return 0;
+}
