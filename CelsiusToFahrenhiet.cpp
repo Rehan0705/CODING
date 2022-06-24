7e4a3db249317e491fb8f92f823c9cd58025effa
Rehan0705 <mohammadrazak876@gmail.com>
2022-06-24 21:32:46 +0530

    Update CelsiusToFahrenhiet.cpp
    T

--- CelsiusToFahrenhiet.cpp
+++ CelsiusToFahrenhiet.cpp
@@ -1,1 +1,12 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    float c;
+    cout<<"enter Temperature in Celsius :";
+    cin>>c;
+    float f=(9*c)/5;
+    f +=32;
+    cout<<"Temperature in Fahrenhiet :"<<f;
+}
