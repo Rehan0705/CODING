0d2daf80bcbbfee935be2b5cfc6b03048785c8b3
Rehan0705 <mohammadrazak876@gmail.com>
2022-06-25 18:14:09 +0530

    Update SelectionSort.cpp
    se

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,1 +1,34 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int small,pos,temp;
+    int n[]={30,50,10,20,40};
+    cout<<"Before sorting ::";
+    for(int i=0;i<5;i++)
+    {
+        cout<<" "<<n[i];
+    }
+    for(int i=0;i<5;i++)
+    {
+        small=n[i];
+        pos=i;
+        for(int j=i+1;j<5;j++)
+        {
+            if(n[j]<small)
+            {
+                small=n[j];
+                pos=j;
+            }
+        }
+        temp=n[i];
+        n[i]=n[pos];
+        n[pos]=temp;
+    }
+    cout<<"\n\n After sorting::";
+    for(int i=0;i<5;i++)
+    {
+        cout<<" "<<n[i];
+    }
+}
