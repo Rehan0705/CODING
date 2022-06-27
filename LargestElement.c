cf4a3046dc0cf7ff8a28f847700967faf617b0ea
Rehan0705 <mohammadrazak876@gmail.com>
2022-06-27 11:14:28 +0530

    Update LargestElement.c
    l

--- LargestElement.c
+++ LargestElement.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
 
+int lag_ele(int arr[],int num)
+{
+    int max_ele;
+    max_ele=arr[0];
+    for(int i=1;i<num;i++)
+        if(arr[i]>max_ele)
+            max_ele=arr[i];
+
+    return max_ele;
+}
+int main()
+{
+    int arr[]={1,24,39,455};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    printf("Largest element of array is %d",lag_ele(arr,n));
+    return 0;
+}
