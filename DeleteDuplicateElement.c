dc318fa36a59e86e6602a37ffab39a864cf78bf8
Rehan0705 <mohammadrazak876@gmail.com>
2022-06-28 12:42:40 +0530

    Update DeleteDuplicateElement.c
    d

--- DeleteDuplicateElement.c
+++ DeleteDuplicateElement.c
@@ -1,1 +1,34 @@
+#include<stdio.h>
 
+int main()
+{
+    int arr[10],size;
+    printf("Enter number of elements :");
+    scanf("%d",&size);
+    printf("\nEnter %d element\n",size);
+    for(int i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<size;i++)
+    {
+        for(int j=i+1;j<size;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                for(int k=j;k<size;k++)
+                {
+                    arr[k]=arr[k+1];
+                }
+                size--;
+                j--;
+            }
+        }
+    }
+    printf("\nFinal array :");
+    for(int i=0;i<size;i++)
+    {
+        printf("%d\n",arr[i]);
+    }
+    return 0;
+}
