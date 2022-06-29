44594ad154cfad89e4d5542c30ecd0f662251a0b
Rehan0705 <mohammadrazak876@gmail.com>
2022-06-29 19:50:33 +0530

    Update CheckCharacter.c
    c

--- CheckCharacter.c
+++ CheckCharacter.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+    char ch;
+    printf("Press any key:");
+    ch=getchar();
+    if(isalpha(ch)>0)
+        printf("The character is a letter.");
+    else
+        if(isdigit(ch)>0)
+            printf("The character is a digit.");
+    else
+        printf("The character is not alphanumeric.");
+        
+}
