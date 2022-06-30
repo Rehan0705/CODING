944b6b9e2406c7b939301362a4fb86a3d9192973
Rehan0705 <mohammadrazak876@gmail.com>
2022-06-30 19:45:30 +0530

    Update FirstAndLast.cpp
    f

--- FirstAndLast.cpp
+++ FirstAndLast.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int a[50],size;
+    cout<<"Enter array size(Max:50):";
+    cin>>size;
+    cout<<"\nEnter array element :";
+    for(int i=0;i<size;i++)
+    {
+        cout<<"\nEnter arr["<<i<<"]element :";
+        cin>>a[i];
+    }
+    cout<<"\nFirst number :"<<a[0]<<endl;
+    cout<<"\nLast number :"<<a[size-1]<<endl;
+
+    return 0;
+}
