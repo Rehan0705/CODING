f4d44fffcc4e50250c0dafe6b9b2dc92ef622839
Rehan0705 <mohammadrazak876@gmail.com>
2022-07-02 20:29:58 +0530

    Update HCFAndLCM.cpp
    hl

--- HCFAndLCM.cpp
+++ HCFAndLCM.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
 
+int main()
+{
+    int a,b,c;
+    cout<<"Enter first number :";
+    cin>>a;
+    cout<<"Enter second number :";
+    cin>>b;
+    c=a*b;
+    while(a!=b)
+    {
+        if(a>b)
+            a=a-b;
+        else
+            b=b-a;
+        
+    }
+    cout<<"HCF :"<<a<<endl;
+    cout<<"LCM :"<<c/a<<endl;
+    return 0;
+}
