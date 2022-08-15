61342f50c0940792b75db21d9907fc1ccbe90566
Rehan0705 <mohammadrazak876@gmail.com>
2022-08-15 21:22:59 +0530

    Update TwoRepeatingElementsInAnArray.cpp
    e

--- TwoRepeatingElementsInAnArray.cpp
+++ TwoRepeatingElementsInAnArray.cpp
@@ -1,1 +1,36 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+  
+void printTwoRepeatNumber(int arr[], int size)
+{
+    int i, j, display=0;
+    int visited[size];
+    for(i = 0; i < size; i++)
+    {
+    if (visited[i] == 1)
+    {
+        continue;
+    }
+    int count = 0;
+        for(j = i + 1; j < size; j++)
+        {
+        if(arr[i] == arr[j])
+        {
+            visited[j] = 1;
+            ++count;
+            break;
+        }
+        }
+        if ( (count > 0) && (display < 2)){
+            ++display;
+        cout<<"repeating element = "<< arr[i]<<endl;
+        }
+    }
+}
+  
+int main()
+{
+    int arr[] = {4, 2, 5, 2, 3, 3, 4, 4, 1, 1};
+    int arr_size = sizeof(arr)/sizeof(arr[0]);
+    printTwoRepeatNumber(arr, arr_size);
+}
